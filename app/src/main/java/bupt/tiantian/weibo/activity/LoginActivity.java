08449d3d3467dbd2715bb1988ac0e4fc24f3e07e
xietianrui <xtr@bupt.edu.cn>
2016-06-15 18:19:40 +0800

    集成了WeiboSDK

---
+++ app/src/main/java/bupt/tiantian/weibo/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package bupt.tiantian.weibo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import bupt.tiantian.weibo.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
